6acbfeb31b4d2208578ff25ad1712deb41f91cd3
Vincent Finn <vincent.s.finn@gmail.com>
2018-12-05 16:24:00 -0500

    first bit completed

---
+++ RPGLecture/RPGProject/Assets/Workspace/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Layer
+{
+    Walkable = 9,
+    Enemy = 10,
+    RaycastEndStop = -1,
+}
+
+public class Utility : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
